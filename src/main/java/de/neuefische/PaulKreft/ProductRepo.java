19b2bd000b44269fd2126e42ba73a5dbe5288e6d
PaulKreft <64103063+PaulKreft@users.noreply.github.com>
2023-12-11 15:30:22 +0100

    add methods to add, remove, and get products (single product and all products)

--- src/main/java/de/neuefische/PaulKreft/ProductRepo.java
+++ src/main/java/de/neuefische/PaulKreft/ProductRepo.java
@@ -4,4 +4,29 @@ import java.util.List;
 
 public class ProductRepo {
     private List<Product> products;
+
+    public void addProduct(Product newProduct) {
+        products.add(newProduct);
+    }
+
+    public void removeProducts(Product newProduct) {
+        products.remove(newProduct);
+    }
+
+    public Product getProductById(String id) {
+        List<Product> filteredById = products.stream().filter(product -> product.id().equals(id)).toList();
+
+        if(filteredById.isEmpty()) {
+            System.out.println("No product found with id " + id);
+            return null;
+        }
+
+        return filteredById.get(0);
+    }
+
+    public List<Product> getAllProduct(Product newProduct) {
+        return products;
+    }
+
+
 }
